33ae0967e21aa7a3cfc0567cb2a8bbd7c83dd20b
thangasadha <thangasadha5@gmail.com>
2021-10-08 09:36:00 +0530

    added system

--- F/src/org/sample/AxisBank.java
+++ F/src/org/sample/AxisBank.java
@@ -4,6 +4,8 @@ public class AxisBank {
 public void savings() {
 	// TODO Auto-generated method stub
 System.out.println("Savings rate is 12 %");
+System.out.println("Thanga rate is 12 %");
+
 }
 public void deposit() {
 	// TODO Auto-generated method stub
