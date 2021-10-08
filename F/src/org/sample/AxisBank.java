37e44f47600e75ce191d911686f1cc509aa1fc28
thangamurugan1 <92135217+thangamurugan1@users.noreply.github.com>
2021-10-08 09:34:35 +0530

    Update AxisBank.java

--- F/src/org/sample/AxisBank.java
+++ F/src/org/sample/AxisBank.java
@@ -8,5 +8,6 @@ System.out.println("Savings rate is 12 %");
 public void deposit() {
 	// TODO Auto-generated method stub
 	System.out.println("Deposit rate is 10%");
+	System.out.println("Deposit...Thanga");
 }
 }
